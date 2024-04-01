9c5fb2e2623d4cc6d83310b3dc383cae5cb7dc5f
Vinh Nguyen <vinhnguyen2308@gmail.com>
2024-04-02 01:22:41 +0700

    Update app icon

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown(
 msgs = StreamlitChatMessageHistory()
 
 with st.container():
-    col1, col2 = st.columns([0.22, 0.8])
+    col1, col2 = st.columns([0.3, 0.8])
     with col1:
         st.image(
             "./assets/app_icon.png", use_column_width="always", output_format="PNG"
